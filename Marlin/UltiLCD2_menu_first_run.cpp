9ee4b12a745f1bf7b95d0c32e136449d2b30086d
Bas van Deursen <b.vandeursen@ultimaker.com>
2015-06-20 12:05:48 +0200

    changed position of printhead during material loading
    The arrow on the printhead was not visible during filament loading; it
    was hidden behind the slider block. This is solved by moving the
    printhead to the middle of the build platform during the material change
    procedure.

--- Marlin/UltiLCD2_menu_first_run.cpp
+++ Marlin/UltiLCD2_menu_first_run.cpp
@@ -288,7 +288,7 @@ static void lcd_menu_first_run_bed_level_paper_right()
 static void parkHeadForHeating()
 {
     lcd_material_reset_defaults();
-    enquecommand_P(PSTR("G1 F12000 X110 Y10"));
+    enquecommand_P(PSTR("G1 F12000 X100 Y100"));
     enquecommand_P(PSTR("M84"));//Disable motor power.
 }
 
